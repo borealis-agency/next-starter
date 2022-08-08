475ba1cb393a0e8d73af13d635a0e46397bd4214
Marko Antolić <marko.antolic01@gmail.com>
2022-08-08 21:17:30 +0200

    Annotate more configuration files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // In React v18, useEffect runs twice on purpose in development mode.
+  // It's a technique to surface bugs due to missing cleanup functions in effects.
+  // Do not disable React Strict Mode just to avoid that behavior!
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
