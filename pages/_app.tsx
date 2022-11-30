e7eb991b0413a9c575f5c06a081b5e224fc02dc3
Marko Antolić <marko.antolic01@gmail.com>
2022-11-30 22:52:50 +0100

    Disable windows focus refetch for react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import "@/styles/globals.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
