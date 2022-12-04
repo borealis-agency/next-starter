fcccaf6cf21875973cbbc4b059220874113755ad
Marko Antolić <marko.antolic01@gmail.com>
2022-12-04 23:09:27 +0100

    Automatically commit husky initialization

--- template/scripts/postClone.mjs
+++ template/scripts/postClone.mjs
@@ -1,4 +1,5 @@
-import { exec, spawnSync } from "child_process";
+/* eslint-disable no-restricted-syntax */
+import { spawnSync } from "child_process";
 import { existsSync, writeFileSync } from "fs";
 import { createRequire } from "module";
 import path from "path";
@@ -7,9 +8,9 @@ import { ensureFolderExists, projectRootFolder } from "./utility.mjs";
 
 const huskyFolderPath = path.resolve(projectRootFolder, ".husky");
 const preCommitHookPath = path.resolve(huskyFolderPath, "pre-commit");
-const isHuskyPreCommitPresent = existsSync(preCommitHookPath);
+const isHuskyPreCommitFilePresent = existsSync(preCommitHookPath);
 
-if (isHuskyPreCommitPresent) {
+if (isHuskyPreCommitFilePresent) {
   console.log();
   console.log("husky pre-commit hook seems to be installed already. That means you probably ran this script before. No files were created or updated now.");
   console.log();
@@ -17,6 +18,7 @@ if (isHuskyPreCommitPresent) {
   const require = createRequire(import.meta.url);
   const packageJsonPath = path.resolve(projectRootFolder, "package.json");
   const packageJsonData = require(packageJsonPath);
+  // Add husky script to scripts section of package.json
   packageJsonData.scripts["prepare"] = "husky install";
 
   ensureFolderExists(huskyFolderPath);
@@ -29,12 +31,17 @@ npx lint-staged
 `,
     { encoding: "utf-8" }
   );
+  // Format package.json with 2 spaces when writing to it
   writeFileSync(packageJsonPath, JSON.stringify(packageJsonData, null, 2) + "\n", { encoding: "utf-8" });
   spawnSync("npm", ["install"], {
     cwd: projectRootFolder,
+    // Show command output in terminal
     stdio: [process.stdin, process.stdout, process.stderr],
     encoding: "utf-8",
   });
+
+  spawnSync("git", ["add", "-A"]);
+  spawnSync("git", ["commit", "-m", "Initialize git hooks using husky"]);
 
   console.log();
   console.log("Post clone script successfully completed!");
