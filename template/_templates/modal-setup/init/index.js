583ff1e126c8cf3f4a3e9b6706eebfe4dac9e39c
Marko Antolić <marko.antolic01@gmail.com>
2023-02-20 20:10:40 +0100

    Add install step for nice-modal-react

---
+++ template/_templates/modal-setup/init/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-syntax */
+const path = require("path");
+const childProcess = require("child_process");
+
+const projectRootFolder = path.resolve(__dirname, "../../../");
+
+console.log("Running initial modal setup...");
+console.log();
+
+childProcess.spawnSync("npm", ["install", "@ebay/nice-modal-react"], {
+  cwd: projectRootFolder,
+  // Show command output in terminal
+  stdio: [process.stdin, process.stdout, process.stderr],
+  encoding: "utf-8",
+});
+
+module.exports = {
+  params: () => {
+    return {};
+  },
+};
