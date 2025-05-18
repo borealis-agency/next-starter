ce0b3aeb2a96898c25a72155bf62c111e165444d
Marko Antolić <22904204+marko-hologram@users.noreply.github.com>
2025-05-18 21:57:19 +0200

    Fix formatting

--- template/src/utility/classes.ts
+++ template/src/utility/classes.ts
@@ -4,9 +4,9 @@ import { extendTailwindMerge } from "tailwind-merge";
 // Here we define a custom tailwind-merge config so that `mergeClassNames` can properly merge our custom Tailwind theme classes. Make sure to update this config when adding new values to the custom Tailwind theme in `tailwind.config.ts`.
 // Source: https://github.com/dcastil/tailwind-merge/blob/v2.6.0/docs/recipes.md
 const customTwMerge = extendTailwindMerge({
-  override: {},
+	override: {},
 });
 
 export const mergeClassNames = (...args: ClassValue[]): string => {
-  return customTwMerge(clsx(args));
+	return customTwMerge(clsx(args));
 };
